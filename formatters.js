9b653241f951fc43a07d4f4ad9453fcaf3dc33e9
Risa <risa.fletcher@gmail.com>
2021-05-08 16:41:46 -0700

    remove kubernetes stuff for now

--- formatters.js
+++ formatters.js
@@ -1,4 +1,20 @@
-const getReferences = require('./getReferences');
+const axios = require('axios');
+
+const WIKIPEDIA_API_BASE_URL = 'http://en.wikipedia.org/w/api.php?';
+
+const getReferences = async ({ title, pageid }, limit = 5) => {
+    const wikiResponse = await axios.get(WIKIPEDIA_API_BASE_URL, {
+        params: {
+            action: 'query',
+            prop: 'extlinks',
+            ellimit: limit,
+            titles: title,
+            format: 'json',
+        },
+    });
+    const extLinks = wikiResponse.data.query.pages[pageid].extlinks;
+    return Array.isArray(extLinks) ? extLinks.map((link) => link['*']) : null;
+};
 
 const generateReferencesResult = async (page = {}, limit) => ({
     id: page.pageid,
