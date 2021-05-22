2f4f01a5c39f1dfd078732d5531d4d59da22f647
Risa <risa.fletcher@gmail.com>
2021-05-22 11:09:58 -0700

    add cors

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const wiki = require('wikipedia');
 const app = express();
-
+app.use(cors());
 const {
     generateReferencesResult,
     generateSummaryResult,
